518a3226cc49ff3b5fbd77110507ff301e3868f1
Debora01-star <debora0805@gmail.com>
2022-10-12 19:12:22 +0200

    family tree

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,4 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+
+    @Test
+    void addChild() {
+    }
 }
---
+++ src/test/java/main/java/PersonTest.java
@@ -0,0 +1,64 @@
+package main.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void addSiblings() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void addParents() {
+    }
+
+    @Test
+    void getPets() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setMiddleName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void setSex() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void setMother() {
+    }
+
+    @Test
+    void setFather() {
+    }
+
+    @Test
+    void setSiblings() {
+    }
+
+    @Test
+    void setChildren() {
+    }
+
+    @Test
+    void setPets() {
+    }
+}
